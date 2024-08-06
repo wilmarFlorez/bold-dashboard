f8f46a5a940425789c8f3f74567ce4f5ad1d2392
Wilmar Florez <wilmar.florez@elenas.la>
2024-08-05 22:10:36 -0500

    [progress] add tooltip

--- components/stat-card/stat-card.tsx
+++ components/stat-card/stat-card.tsx
@@ -1,7 +1,14 @@
-import Image from 'next/image'
-import { Card, CardContent, CardHeader, CardTitle } from '../ui/card'
-import Icon from '../icon/icon'
+'use client'
 
+import { useState } from 'react'
+import { Card, CardContent, CardHeader, CardTitle } from '../ui/card'
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip'
 interface StatCardProps {
   title: string
   value: string
@@ -10,12 +17,25 @@ interface StatCardProps {
 }
 
 export const StatCard = ({ title, value, date, icon }: StatCardProps) => {
+  const [openTooltip, setOpenTooltip] = useState(false)
+
+  const handleClickInfo = () => {
+    setOpenTooltip((prevState) => !prevState)
+  }
+
   return (
     <Card className='shadow-md border-none rounded-2xl overflow-hidden'>
       <CardHeader className='bg-blue-500 bg-main-gradient text-light-gray'>
         <div className='flex justify-between'>
           <CardTitle className='font-normal text-base'>{title}</CardTitle>
-          {icon}
+          <TooltipProvider>
+            <Tooltip open={openTooltip}>
+              <TooltipTrigger onClick={handleClickInfo}>{icon}</TooltipTrigger>
+              <TooltipContent className='bg-white text-dark-gray'>
+                <p className='text-xs'>Resumen de ventas</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
         </div>
       </CardHeader>
       <CardContent className='bg-white pt-5 pb-2 px-2 flex flex-col gap-2'>
